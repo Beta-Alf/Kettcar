952392b6f9a84055088eed5657e630ae0c28219a
Florian Wagner <florian.bonn@gmail.com>
2020-02-19 21:09:31 +0100

    feat: Add the basic project structure
    This commit adds an initial python file to establish a basic project
    structure.

---
+++ kettcar/__init__.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+""" Kettcar is a python module to parse git histories for conventional commit
+messages. It can then generate SemVer versions based on the occurences of these
+commit messages.
+"""
+
+if __name__ == '__main__':
+    print('Welcome to kettcar, nothing has been implemented yet')
