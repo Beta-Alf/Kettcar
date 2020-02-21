baa40779ecd32e64a6b5522bfac157d8c22f1a96
Florian Wagner <florian.wagner@student.hpi.de>
2020-02-21 10:01:51 +0100

    feat: Parse commit message with regex
    This commit introduces basic functionality to parse commit messages
    using python regexes.

--- kettcar/__init__.py
+++ kettcar/__init__.py
@@ -4,9 +4,20 @@ messages. It can then generate SemVer versions based on the occurences of these
 commit messages.
 """
 import git
+import re
+
+
+def parse_commit_message(message):
+    minor_re = re.compile("\Afeat:")
+    if minor_re.match(message):
+        print("matched a feature")
+
 
 if __name__ == '__main__':
     commits = list(git.Repo("./").iter_commits())
+    for c in commits[::-1]:
+        parse_commit_message(c.message)
+
     print("{} commits in this repository".format(len(commits)))
 
     print('Welcome to kettcar, nothing has been implemented yet')
